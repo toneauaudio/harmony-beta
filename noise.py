5324351deee3bb04bc88003f5bb1f29c4fdbb61a
mcollins21 <mcollins21@luc.edu>
2024-01-12 05:10:21 -0500

    created noise generator

---
+++ noise.py
@@ -0,0 +1,10 @@
+from pyo import *
+
+def noiseGen():
+
+    n1 = Noise(0.3)
+    n2 = PinkNoise(0.3)
+    n3 = BrownNoise(0.3)
+
+    sel = Selector([n1, n2, n3]).out()
+    sel.ctrl(title = "Input interpolator (0=White, 1=Pink, 2=Brown)")
