5f0c0006cc55e700edcca983d08dd4481823b7eb
mcollins21 <mcollins21@luc.edu>
2024-01-11 22:37:09 -0500

    added edge case check for no devices

--- midiUtils.py
+++ midiUtils.py
@@ -1,14 +1,18 @@
 from pyo import *
 
 def Devices():
+    device_count = pm_count_devices()   # recieving the count of devices
 
-    print("List of found devices: \n")
-    pm_list_devices()   # prints a list of all devices found by portmidi
+    if device_count < 1:    # if no devices are found
+        print("No Devices Found")
+    else:
+        print("List of found devices: \n")
+        pm_list_devices()   # prints a list of all devices found by portmidi
 
-    inputDevices = pm_get_input_devices()  # returns midi input devices found by portmidi
-    print("Input Devices: \n")
-    print(inputDevices)
+        inputDevices = pm_get_input_devices()  # returns midi input devices found by portmidi
+        print("Input Devices: \n")
+        print(inputDevices)
 
-    outputDevices = pm_get_output_devices() # returns midi output devices found by portmidi
-    print("\nOutput Devices: \n")
-    print(outputDevices)
+        outputDevices = pm_get_output_devices() # returns midi output devices found by portmidi
+        print("\nOutput Devices: \n")
+        print(outputDevices)
